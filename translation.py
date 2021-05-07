092f848b29a7559454790e3139ba85dcddb94e27
dakshkohli23 <dakshkohli23@gmail.com>
2021-05-07 17:13:37 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,5 @@
 class Translation(object):
-    START_TEXT = """Hello,
-    This is Roronoa Zoro Rename Bot!
+    START_TEXT = """Hello {}, \n This is Roronoa Zoro Rename Bot!
 
 <b>Please send me any Telegram file and reply to that file to /rename New Name.extension</b>
 
@@ -8,8 +7,8 @@ class Translation(object):
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>⇢ Create Your Own Clone Bot... </b>  /help for Details"
-    DOWNLOAD_START = "📥 Downloading"
-    UPLOAD_START = "📤 Uploading"
+    DOWNLOAD_START = "<b>📥 Downloading</b>"
+    UPLOAD_START = "<b>📤 Uploading</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me. 🤖**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
